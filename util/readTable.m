7c74f198bb049ac49693cad8519452d73f796fd5
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-07-01 12:17:56 +0800

    Update readTable.m: return missing labels and pass negative bbox

--- util/readTable.m
+++ util/readTable.m
@@ -1,27 +1,33 @@
-function dataTable = readTable(filename)
+function [dataTable, missingLabels] = readTable(filename)
     fid = fopen(filename, 'r');
     colNames = cell(1);
     colNamesIdx = 1;
     dataCell = cell(1);
     dataCellRow = 1;
     firstLoop = true;
+    labels = [];
     line = fgetl(fid);
     while ischar(line)
         chunks = strsplit(line, ',');
         if firstLoop  % Read table header
+            labels = zeros(numel(chunks), 1);
             for i = 1:numel(chunks)
                 colNames(colNamesIdx) = chunks(i);
                 colNamesIdx = colNamesIdx + 1;
             end
             firstLoop = false;
         else
+            boxFound = false;
             for i = 1:numel(chunks)
                 str = char(chunks(i));
-                if numel(strfind(str, '-')) ~= 0  % Empty bounding box values
+                if numel(strfind(str, '-')) ~= 0  % Empty bounding box values or negative bounding box
                     dataCell{dataCellRow, i} = cell(1);
                 elseif numel(strfind(str, ' ')) == 0  % Image path
                     dataCell{dataCellRow, i} = chunks(i);
+                    labels(i) = 1;
                 else  % Bounding box values
+                    labels(i) = 1;
+                    boxFound = true;
                     boxVals = strsplit(str, ' ');
                     boxValsCell = cell(1);
                     boxValsRow = 1;
@@ -35,10 +41,13 @@ function dataTable = readTable(filename)
                     dataCell{dataCellRow, i} = boxValsCell;
                 end
             end
-            dataCellRow = dataCellRow + 1;
+            if boxFound
+                dataCellRow = dataCellRow + 1;
+            end
         end
         line = fgetl(fid);
     end
     fclose(fid);
     dataTable = cell2table(dataCell, 'VariableNames', colNames);
+    missingLabels = colNames(find(labels == 0));
 end
